f662ffee9169e4347df4472b6fd211e5e3fc6092
maclandrol <emmanuel.noutahi@hotmail.ca>
2023-04-10 23:30:11 -0400

    add support for non smiles input in embedding

--- graphormer_pretrained/embeddings.py
+++ graphormer_pretrained/embeddings.py
@@ -72,7 +72,9 @@ class GraphormerEmbeddingsExtractor:
 
     def __call__(self, smiles: List[str]):
         """Predict molecular embeddings from a list of SMILES strings"""
-        batch_graphs = self._convert(smiles)
+        batch_graphs =  smiles
+        if isinstance(smiles, (list, str)) and isinstance(smiles[0], str):
+            batch_graphs = self._convert(smiles)
         encoder = self.model.encoder
         inner_states, graph_rep = encoder.graph_encoder(
             batch_graphs,
